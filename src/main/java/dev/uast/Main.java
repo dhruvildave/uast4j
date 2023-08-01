21e7fc8e81734760de37a5889c650775c970d89f
Dhruvil Dave <43331416+dhruvildave@users.noreply.github.com>
2023-08-01 13:07:53 -0500

    Add command-line argument parsing

--- src/main/java/dev/uast/Main.java
+++ src/main/java/dev/uast/Main.java
@@ -1,29 +1,65 @@
 package dev.uast;
 
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
+    static final Set<String> fromSchemes =
+            Set.of("uast", "raw", "devanagari", "iast", "slp", "gu", "or", "ta", "te", "ml", "kn");
+
+    static final Set<String> toSchemes =
+            Set.of("uast", "devanagari", "iast", "gu", "or", "ta", "te", "ml", "kn");
+
     public static void main(String[] args) {
-        var x = "ūrdhvamūlam adhaḥśākham 15";
+        if (args.length % 2 != 0) {
+            throw new IllegalArgumentException("invalid number of arguments");
+        }
 
-        for (var i : new String[] {"devanagari", "gu", "te", "ta", "kn", "ml", "or", "io"}) {
-            var fs = UAST.convertor.get("iast").get(i);
+        var to = "devanagari";
+        var from = "uast";
 
-            if (fs == null) {
-                System.out.println(i + " not found");
+        for (var i = 0; i < args.length; i += 2) {
+            if (args[i].equals("-to")) {
+                if (!toSchemes.contains(args[i + 1])) {
+                    throw new IllegalArgumentException("invalid value for `to`");
+                }
+                to = args[i + 1];
                 continue;
             }
 
-            for (var l : x.split("\\n")) {
-                var s = l.split(" ");
-                var arr = new ArrayList<String>(s.length);
-                for (var w : s) {
-                    for (var f : fs) {
-                        w = f.apply(w);
-                    }
-                    arr.add(w);
+            if (args[i].equals("-from")) {
+                if (!fromSchemes.contains(args[i + 1])) {
+                    throw new IllegalArgumentException("invalid value for `from`");
                 }
-                System.out.println(String.join(" ", arr));
+                from = args[i + 1];
+                continue;
+            }
+
+            if (args[i].matches("-\\w+")) {
+                throw new IllegalArgumentException("illegal argument passed " + args[i]);
+            }
+        }
+
+        var transforms = UAST.convertor.get(from).get(to);
+        try (var in = new Scanner(System.in, StandardCharsets.UTF_8);
+                var out = new PrintWriter(System.out, true)) {
+            while (in.hasNextLine()) {
+                var x = in.nextLine();
+
+                for (var l : x.split("\\n")) {
+                    var s = l.split(" ");
+                    var arr = new ArrayList<String>(s.length);
+                    for (var w : s) {
+                        for (var f : transforms) {
+                            w = f.apply(w);
+                        }
+                        arr.add(w);
+                    }
+                    out.println(String.join(" ", arr));
+                }
             }
         }
     }
