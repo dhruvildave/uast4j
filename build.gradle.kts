c8fbae9e94321241bf85ccf95194be37a87a77af
Dhruvil Dave <43331416+dhruvildave@users.noreply.github.com>
2024-08-22 12:41:13 -0500

    Dependency bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 plugins {
     idea
     application
-    kotlin("jvm") version "2.0.0"
+    kotlin("jvm") version "2.0.20"
 }
 
 group = "dev.uast"
